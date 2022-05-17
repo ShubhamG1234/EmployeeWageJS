a303300c1b77a3584e1d272de4e361ebee5d07fa
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-05-17 20:24:36 +0530

    uc4

--- EmployeeWage/Emp.js
+++ EmployeeWage/Emp.js
@@ -1,17 +1,53 @@
-// UC_3 //
+// UC1 Check Employee Presence
+/// Variable
+const IS_ABSENT = 0;
+/// Generates 0 or 1 randomly.
+let empcheck = Math.floor(Math.random() * 10) % 2;
+/// Checks Employee is present or not.
+if(empcheck == IS_ABSENT)
+{
+    // Prints Employee is absent when random number is 0.
+    console.log("Employee is Absent.");
+    return;
+}
+else
+{
+    // Prints Employee is Present when random number is 1.
+    console.log("Employee is Present.");
+}
 
-function getWorkingHours(empCheck){
-    switch (empCheck){
+
+// UC 2 Check for daily wage based on wheather the employee is part time or full time
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const TotalWorkingDays = 20;
+
+function GetWorkingHours(employeecheck)
+{
+switch(employeecheck)
+    {
         case IS_PART_TIME:
             return PART_TIME_HOURS;
-            case IS_FULL_TIME:
+            break;
+        case IS_FULL_TIME:
             return FULL_TIME_HOURS;
-            default:
-                return 0;
+            break;
+        default:
+            return 0;
     }
 }
-let empHrs =0;
-let empCheck = Math.floor(Math.random()*10) % 3;
-empHrs = getWorkingHours(empCheck);
+let empHrs = 0;
+
+
+// UC4 Calculate Employee wages for a month
+for (let day = 0; day < TotalWorkingDays; day++) {
+    let employeecheck = Math.floor(Math.random() * 10) % 3;    
+    empHrs += GetWorkingHours(employeecheck);    
+}
+// Calculates Employee wage for a day.
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage:" + empWage);
+/// Prints Employee wage.
+console.log("Employee wage is: " + empWage);
